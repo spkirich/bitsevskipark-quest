4f535beb92172dd4251e0bba3b4fb2e380c0895f
Sergey Kirichenko <s2002kir@yandex.ru>
2019-12-24 18:21:17 +0300

    Fix Android-specific bug

--- rivers.js
+++ rivers.js
@@ -2,25 +2,36 @@ var rivers = ["ЧЕРТАНОВКА", "ГОРОДНЯ", "ДУБИНКИНСКА�
 var fields = Array(rivers.length);
 
 for (var i = 0; i < fields.length; i++) {
-  fields[i]           = document.getElementById("field_" + i);
-  fields[i].value     = hideLetters(rivers[i]);
-  fields[i].onkeydown = handler(i);
-  fields[i].onpaste   = function(event) { event.preventDefault() };
+  fields[i]               = document.getElementById("field_" + i);
+  fields[i].value         = hideLetters(rivers[i]);
+  fields[i].onkeydown     = handleKeydown(i);
+  fields[i].onbeforeinput = handleInput(i);
+  fields[i].onpaste       = suppress;
 }
 
-function handler(i) {
-  var field = fields[i];
-  var river = rivers[i];
-
+function handleKeydown(i) {
   return function(event) {
-    for (var i = 0; i < river.length; i++) {
-      if (river[i] == event.key.toUpperCase()) {
-        field.value = replaceAt(field.value, river[i], i);
-      }
-    }
-
+    processInput(fields[i], rivers[i], event.key.toUpperCase());
     event.preventDefault();
   };
+}
+
+function handleInput(i) {
+  return function(event) {
+    processInput(fields[i], rivers[i], event.data.toUpperCase());
+    event.preventDefault();
+  };
+}
+
+function processInput(field, river, input) {
+  for (var i = 0; i < river.length; i++)
+    for (var j = 0; j < input.length; j++) {
+      if (river[i] == input[j]) field.value = replaceAt(field.value, river[i], i);
+    }
+}
+
+function suppress(event) {
+  event.preventDefault();
 }
 
 function hideLetters(word) {
